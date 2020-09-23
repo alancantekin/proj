9ef28e4c77fba3a27d0325784c3dce56e1c63d7b
Alan Cantekin <alan_mattia.cantekin@roche.com>
2020-09-23 18:24:01 +0200

    param removed

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function get_api(position) {
     });
 }
 
-function search(param){
+function search(){
     let val = document.getElementById("searchValue").value;
     if(val)
         window.open("https://www.google.com/search?q=" + val, "_blank");
